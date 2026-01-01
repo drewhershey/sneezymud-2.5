fe6ff4ce806f9e9c00ec712165b9d9f6228f9435
Drew Hershey <7330404+drewhershey@users.noreply.github.com>
2026-01-01 13:20:51 -0700

    Refactor bitwise operation macros to concepts and constexpr functions

--- code/bit_ops.h
+++ code/bit_ops.h
@@ -1,21 +1,35 @@
 #pragma once
 
-#include <type_traits>
+#include <concepts>
 
-#define IS_SET(flag, bit) ((flag) & (bit))
+// Concept for types that support bitwise operations (integers and enums)
+template <typename T>
+concept BitfieldType = std::integral<T> || std::is_enum_v<T>;
 
-#define SET_BIT(var, bit) \
-  ((var) = (std::remove_reference_t<decltype(var)>)((var) | (bit)))
+template <BitfieldType T, BitfieldType U>
+constexpr bool IS_SET(T flag, U bit) {
+  return (flag & bit) != 0;
+}
 
-#define REMOVE_BIT(var, bit) \
-  ((var) = (std::remove_reference_t<decltype(var)>)((var) & ~(std::remove_reference_t<decltype(var)>)(bit)))
+template <BitfieldType T, BitfieldType U>
+constexpr void SET_BIT(T& var, U bit) {
+  var = static_cast<T>(var | bit);
+}
 
-#define TOGGLE_BIT(var, bit) \
-  ((var) = (std::remove_reference_t<decltype(var)>)((var) ^ (bit)))
+template <BitfieldType T, BitfieldType U>
+constexpr void REMOVE_BIT(T& var, U bit) {
+  var = static_cast<T>(var & ~bit);
+}
 
-#define SWITCH(a, b) \
-  {                  \
-    (a) ^= (b);      \
-    (b) ^= (a);      \
-    (a) ^= (b);      \
-  }
+template <BitfieldType T, BitfieldType U>
+constexpr void TOGGLE_BIT(T& var, U bit) {
+  var = static_cast<T>(var ^ bit);
+}
+
+template <typename T, typename U>
+  requires std::assignable_from<T&, U> && std::assignable_from<U&, T>
+constexpr void SWITCH(T& a, U& b) {
+  T tmp = a;
+  a = static_cast<T>(b);
+  b = static_cast<U>(tmp);
+}
